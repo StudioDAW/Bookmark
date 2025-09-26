2eb631d06d74d65dfaede102f1a460e913bd8ce6
Dylan <github@applesandbananas.co.uk>
2025-09-26 05:32:33 +0100

    forgot to commit bkm.py

--- bkm.py
+++ bkm.py
@@ -1,6 +1,9 @@
+import enum
 import os
+import re
 import sys
 import time
+import math
 import uuid
 import lorem
 from layout import TextLayout, convert, pagesizes
@@ -9,6 +12,8 @@ from typing import Literal, get_type_hints, Union, Tuple
 import freetype
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+from text_metrics import TextMetrics
 
 DPI = 96
 
@@ -40,10 +45,9 @@ class HTMLElement:
         self._tag = tag
         self._classname = classname
         self._style = style if style else {}
-        self._children = {}  # id -> HTMLElement or string
+        self._children = {}  
         self._id = id if id else str(uuid.uuid4())
 
-        # add initial content if provided
         if content:
             if isinstance(content, dict):
                 for name, child in content.items():
@@ -87,17 +91,17 @@ class HTMLElement:
         return self._children
 
     def append(self, element, name=None):
-        """Append a child (HTMLElement or string)."""
+        """Append a child (HTMLElement or string). Ensure it's a proper Unicode string."""
+        if isinstance(element, bytes):
+            element = element.decode("utf-8")
         if isinstance(element, str):
             child_id = name if name else str(uuid.uuid4())
             self._children[child_id] = element
             return child_id
-
         if isinstance(element, HTMLElement):
             child_id = name if name else element.id
             self._children[child_id] = element
             return element.id
-
         raise TypeError("Child must be HTMLElement or string")
 
     def __str__(self):
@@ -145,7 +149,7 @@ class BKM:
                  start:     Literal["left","right"] = "right",
                  # margin:    Union[int, float, Tuple[int|float, ...]] = (25,25,30,25),
                  margin:    Tuple[int|float, ...] = (25,25,30,25),
-                 bleed:     Tuple[int|float, ...] = (3,)):
+                 bleed:     Tuple[int|float, ...] = (0,)):
         w, h = pagesizes[size][unit]
         if width is not None: w = width
         if height is not None: h = height
@@ -185,10 +189,9 @@ class BKM:
     def page(self):
         page = Div("page", style={"width": f"{self.size[0]+self.bleed[1]+self.bleed[3]}{self.unit}", "height": f"{self.size[1]+self.bleed[0]+self.bleed[2]}{self.unit}", "background-color": "white", "position": "relative", "padding": " ".join([f"{m+self.bleed[i]}{self.unit}" for i, m in enumerate(self.margin)]), "box-sizing": "border-box", "line-height": "1.2"})
         self.pages.append(page)
-        self.cursor_y = self.margin[0]+self.bleed[0]
+        self.cursor_y = (self.margin[0]+self.bleed[0]) * convert[self.unit]["px"]
 
-    def paragraph(self, content: str, width: float = 1.0, font: tuple[str,int] = ("Helvetica", 20)):
-        print(font)
+    def paragraph(self, content: str, width: float = 1.0, font: tuple[str,int] = ("Helvetica", 16)):
         default_width = self.size[0] - self.margin[1] - self.margin[3]
         if width == 1.0:
             width = default_width
@@ -197,53 +200,89 @@ class BKM:
         elif width < 0:
             width += default_width
 
-        layout = TextLayout(content, "/System/Library/Fonts/Helvetica.ttc", font[1])
+        # words = re.findall(r'\S+\s*', content)
+        words = content.split()
 
-        height = layout.height(max_width=width * convert[self.unit]["px"])
+        text_metrics = TextMetrics(font)
+        word_widths, space_width = text_metrics.measure_words(words)
+        line_height = text_metrics.line_height()
 
-        print(height, height * convert["px"][self.unit])
-        self.cursor_y += height * convert["px"][self.unit]
+        line_length = 0
+        lines = []
+        line = []
+        height = 0
+        split = []
 
-        newpage = False
-        print("y:",self.cursor_y)
-        print("s:",self.size[1]  - self.margin[2] + self.bleed[2])
-        if self.cursor_y > self.size[1] - self.margin[2] + self.bleed[0] + self.bleed[2]:
-            diff = self.cursor_y - (self.size[1] - self.margin[2] + self.bleed[0] + self.bleed[2])
-            line_height = font[1] * convert["pt"]["px"] * 1.2
-            lines = int((diff * convert[self.unit]["px"]) / line_height)
-            split = content.splitlines()
-            self.cursor_y -= height * convert["px"][self.unit] + line_height
-            self.paragraph("\n".join(split[:len(split)-lines]), font=font)
-            self.page()
-            content = "\n".join(split[len(split)-lines:])
+        max_width = width * convert[self.unit]["px"]
+        max_height = (self.size[1] - self.margin[0] - self.margin[2]) * convert[self.unit]["px"]
+        for word in words:
+            word_width = word_widths[word]
+            line_length += word_width + space_width
+            if line_length - space_width <= max_width:
+                line.append(word)
+            else:
+                height += line_height
+                if height > max_height:
+                    height = line_height
+                    split.append(" ".join(lines))
+                    lines = []
+                lines.append(" ".join(line))
+                print(lines[-1])
+                line = [word]
+                line_length = word_width + space_width
+        lines.append(" ".join(line))
+        split.append(" ".join(lines))
 
-        content = "</br>".join(content.splitlines())
-        paragraph = P("paragraph",
-                      style={"margin-top": "0",
-                             "font-family": font[0],
-                             "font-size": f"{layout.font_size_pt}pt",
-                             "text-align": "justify",
-                             "font-kerning": "none"})
-        paragraph.style["width"] = str(width)+self.unit
-        paragraph.append(content)
-        self.pages[-1].append(paragraph)
+        for i, text in enumerate(split):
+            paragraph = P(classname="paragraph",
+                          style={"margin-top": "0",
+                                 "margin-bottom": "0",
+                                 "width": f"{width}{self.unit}",
+                                 "font-family": font[0],
+                                 "font-size": f"{font[1]}pt",
+                                 "text-align": "justify"},
+                          content=text)
+            self.pages[-1].append(paragraph)
+            if i < len(split)-1:
+                self.page()
+        self.cursor_y += height
+        print(self.cursor_y)
 
     def lorem(self, paragraphs=1, width=1):
         for i in range(paragraphs):
             self.paragraph(lorem.paragraph(), width)
 
     def write(self):
-        print(self.margin)
         if self.spread and self.start == "right":
             self.pages[0].style["grid-column"] = "2"
+
         if self.guides:
             for page in self.pages:
                 page.append(Div("guide", {"position": "absolute", "outline": "1px solid blue", "inset": " ".join([f"{m+self.bleed[i]}mm" for i, m in enumerate(self.margin)])}))
                 page.append(Div("guide", {"position": "absolute", "outline": "1px solid darkgrey", "inset": " ".join([f"{b}mm" for b in self.bleed])}))
+
         for page in self.pages:
             self.view.append(page)
-        with open("index.html", "w") as f:
-            f.write('<script src="http://localhost:35729/livereload.js"></script>'+str(self.view))
+
+        html_start = """<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <script src="http://localhost:1234/livereload.js"></script>
+    </head>
+    """
+
+        html_end = """
+    </html>
+    """
+
+        # Convert the Body object to string safely
+        body_content = str(self.view)
+
+        with open("index.html", "w", encoding="utf-8") as f:
+            f.write(html_start + body_content + html_end)
+
+        print("write")
 
 
 
